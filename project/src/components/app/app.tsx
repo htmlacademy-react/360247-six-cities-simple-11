75c20822faaa3cd2328f9e026bd7b3b931dd84aa
Eyespot <eger_np_@inbox.ru>
2022-10-24 23:48:59 +0300

    adds pages, ScreenMain components

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,31 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import ScreenAuth from '../../pages/screen-auth/screen-auth';
+import ScreenMain from '../../pages/screen-main/screen-main';
+import ScreenOffer from '../../pages/screen-offer/screen-offer';
+
+type AppScreenProps = {
+  offersData: {
+    title: string;
+    imageUrl: string;
+    type: string;
+    price: number;
+    ratingInPercents: number;
+    isPremium: boolean;
+    id: number;
+  }[];
+}
+
+function App({offersData}: AppScreenProps): JSX.Element {
+  return (
+    <>
+      <ScreenMain
+        offersData = {offersData}
+      />
+
+      <ScreenAuth />
+
+      <ScreenOffer />
+    </>
+  );
 }
 
 export default App;
