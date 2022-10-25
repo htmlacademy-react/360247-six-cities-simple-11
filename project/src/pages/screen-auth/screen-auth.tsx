a288953fdf62689cc90799ce36a5ea712bc878f5
Eyespot <eger_np_@inbox.ru>
2022-10-25 09:48:30 +0300

    fixes mistakes

--- project/src/pages/screen-auth/screen-auth.tsx
+++ project/src/pages/screen-auth/screen-auth.tsx
@@ -2,11 +2,9 @@ import Header from '../../components/header/header';
 
 function ScreenAuth(): JSX.Element {
 
-  const isHeaderAuthToBeRendered = false;
-
   return (
     <div className="page page--gray page--login">
-      <Header isHeaderAuthToBeRendered={isHeaderAuthToBeRendered} />
+      <Header isHeaderAuthToBeRendered={false} />
       <main className="page__main page__main--login">
         <div className="page__login-container container">
           <section className="login">
