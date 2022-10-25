404183caf07264fdc6f67b5ef099f9e0b3b734ea
Eyespot <eger_np_@inbox.ru>
2022-10-25 13:41:15 +0300

    adds ScreenAuth components

---
+++ project/src/components/shortcut-to-offers/shortcut-to-offers.tsx
@@ -0,0 +1,14 @@
+function ShortcutToOffers(): JSX.Element {
+
+  return (
+    <section className="locations locations--login locations--current">
+      <div className="locations__item">
+        <a className="locations__item-link" href="#">
+          <span>Amsterdam</span>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default ShortcutToOffers;
